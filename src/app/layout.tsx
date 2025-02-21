e5269c717cf22617bc3c7d5d2ec3a25b57cb8ccb
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-02-21 22:29:42 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,9 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: 'Frontend Mentor - Stats preview card component',
+  title: 'Arda Eker - Stats preview card component',
   description:
-    'This website is a solution for the Stats preview card component challenge on Frontend Mentor. It is built with Next.js and Tailwind CSS. Coded by Arda Eker.',
+    'An accessible and responsive stats card with semantic HTML and metadata optimization.',
   authors: {
     name: 'Arda Eker',
     url: 'https://github.com/ardaeker',
@@ -43,10 +43,10 @@ export const metadata: Metadata = {
   ],
   metadataBase: new URL('https://stats-preview-card-component-sage-one.vercel.app/'),
   openGraph: {
-    title: 'Frontend Mentor - Stats preview card component',
+    title: 'Arda Eker - Stats preview card component',
     description:
-      'This website is a solution to the Stats preview card component challenge on Frontend Mentor. It is built with Next.js, Tailwind CSS, and TypeScript. Coded by Arda Eker.',
-    siteName: 'Frontend Mentor - Stats preview card component',
+      'An accessible and responsive stats card with semantic HTML and metadata optimization.',
+    siteName: 'Arda Eker - Stats preview card component',
     images: [
       {
         url: 'opengraph-image.png',
@@ -55,9 +55,6 @@ export const metadata: Metadata = {
   },
   alternates: {
     canonical: 'https://stats-preview-card-component-sage-one.vercel.app',
-  },
-  icons: {
-    icon: '/favicon.png',
   },
 };
 
